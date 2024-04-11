2cc780cb66958f56c5ed5fdcdbcdd611b6faa3ea
IsidroGlez <isidro@fintecimal.com>
2024-04-10 23:55:28 -0600

    Se implemento chatgpt.

--- src/utils/services/controller/utilsController.ts
+++ src/utils/services/controller/utilsController.ts
@@ -3,6 +3,7 @@ import { StatusCodes } from 'http-status-codes';
 import DataResponse from '../../../utils/responses/DataResponse';
 import ErrorResponse from '../../../utils/errors/ErrorResponse';
 import SendWhatsapp from '../../whatsappDeliver/SendWhatsapp';
+import OpenAIChat from '../../textGenerator/OpenAIChat';
 
 export default class UtilsController {
   async sendWhatsapp(req: Request, res: Response, next: NextFunction) {
@@ -12,6 +13,19 @@ export default class UtilsController {
       const { dataMessage, templateMessage, phone } = item;
       const user = await sendWhatsapp.exec(dataMessage, templateMessage, phone);
       const returnValue = new DataResponse({user});
+      return res.status(StatusCodes.CREATED).json(returnValue);
+    }
+    catch (e) {
+      const returnValue = new ErrorResponse({ message: e.message, error: e });
+      return res.status(StatusCodes.UNAUTHORIZED).json(returnValue);
+    }
+  }
+  async openAi(req: Request, res: Response, next: NextFunction) {
+    try {
+      const { message } = req.body as any;
+      const openAIChat = new OpenAIChat();
+      const chat = await openAIChat.exec(message);
+      const returnValue = new DataResponse({ chat });
       return res.status(StatusCodes.CREATED).json(returnValue);
     }
     catch (e) {
